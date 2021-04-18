8d304f103364042f982d5b41cd864aa10c2185c3
Pavitra <ssharmapavitra@gmail.com>
2021-04-19 00:16:50 +0530

    Official Website Uploaded

---
+++ common.php
@@ -0,0 +1,9 @@
+<?php
+//For Development
+//$con= mysqli_connect("localhost","root","","malhar")
+
+$con= mysqli_connect("remotemysql.com","IUQ8DZ1ksO","3HFAXtlYBM","IUQ8DZ1ksO")
+
+or die(mysqli_error($con));
+
+?>
