3045cd7560a6fb05acebec113ab3688bfcb38b1a
ssharmapavitra <80170208+ssharmapavitra@users.noreply.github.com>
2022-01-08 00:37:48 +0530

    Update common.php

--- common.php
+++ common.php
@@ -1,6 +1,6 @@
 <?php
 //For Development
-$con= mysqli_connect("localhost","root","","malhar")
+//$con= mysqli_connect("localhost","root","","malhar")
 
 //$con= mysqli_connect("remotemysql.com","IUQ8DZ1ksO","3HFAXtlYBM","IUQ8DZ1ksO")
 
